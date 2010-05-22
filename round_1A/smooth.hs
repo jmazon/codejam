5650be7f3684a9b9a7552f5fa751d20bd4cb3a1f
Jean-Baptiste Mazon <jb@patou>
2010-05-22 15:59:55 +0200

    Slight speed improvement with UArray and ST

--- round_1A/smooth.hs
+++ round_1A/smooth.hs
@@ -1,6 +1,8 @@
-import Data.Array
+import Data.Array.Unboxed
+import Data.Array.ST
 import Control.Applicative
 import Control.Monad
+import Control.Monad.ST
 import System.IO
 
 main = do
@@ -11,16 +13,24 @@ main = do
     ns <- map read . words <$> getLine
     putStrLn $ "Case #" ++ show j ++ ": " ++ show (solve d i m n ns)
 
---solve :: Int -> Int -> Int -> Int -> [Int] -> Int
-solve d i m n ns = minimum [ s n f | f <- [0..255] ]
-    where a = listArray (1,n) ns
-          c = listArray ((0,0),(n,255))
-              [ s' n' f | n' <- [0..n], f <- [0..255] ]
-          s n f = c ! (n,f)
-          s' 0 f             = 0
-          s' n f | m > 0     = minimum $ s (n-1) f + d :
-                               [ s (n-1) p +
-                                 abs (f - a!n) +
-                                 i * max 0 ((abs (f - p) - 1) `div` m)
-                                 | p <- [0..255] ]
-                 | otherwise = s (n-1) f + min d (abs (f - a!n))
+solve :: Int -> Int -> Int -> Int -> [Int] -> Int
+solve d i m n ns = minimum [ c!(n,f) | f <- [0..255] ]
+    where a = listArray (1,n) ns :: UArray Int Int
+          c = runSTUArray $ do
+                t <- newArray_ ((0,0),(n,255))
+                forM_ [0..n] $ \n ->
+                  forM_ [0..255] $ \f -> do
+                    v <- s' t n f
+                    writeArray t (n,f) v
+                return t
+          s t n f = readArray t (n,f)
+          s' t 0 f             = return 0
+          s' t n f | m > 0     = do
+            dc <- s t (n-1) f
+            pc <- mapM (s t (n-1)) [0..255]
+            return $ minimum $ (dc + d) :
+                               zipWith (+) [ abs (f - a!n) +
+                                             i * max 0 ((abs (f - p) - 1) `div` m)
+                                             | p <- [0..255] ]
+                                           pc
+                   | otherwise = (+ min d (abs (f - a!n))) <$> s t (n-1) f 
