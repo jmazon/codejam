dce8083c5da555e88f37caeb39cc526431bf4132
Jean-Baptiste Mazon <jb@yavin>
2013-04-25 12:30:00 +0200

    Simplify Treasure solution

--- 2013-0-Qualif/D-treasure.hs
+++ 2013-0-Qualif/D-treasure.hs
@@ -1,8 +1,8 @@
-import Data.Array
-import Data.List (delete,sort)
-import qualified Data.BitSet as B
+import Data.Array    ((!),listArray,accumArray,elems)
+import Data.Maybe    (listToMaybe)
+import Data.List     (delete,sort)
 import Control.Monad (forM_,replicateM,guard)
-import Control.Applicative ((<|>))
+import qualified Data.BitSet as B
 
 readInts = fmap (map read . words) getLine
 
@@ -10,34 +10,29 @@ main = do
   t <- readLn
   forM_ [1..t] $ \i -> do
     [k,n] <- readInts
-    iKeys <- readInts
-    chests <- replicateM n readInts
+    startKeys <- readInts
+    rawChests <- replicateM n readInts
     let chestsOpenedBy = accumArray (++) [] (1,200) $
-                         zip (map head chests) (map (:[]) [1..])
-        keysInChest = listArray (1,n) $ map (tail.tail) chests
-        keyOpening = listArray (1,n) $ map head chests
+                         zip (map head rawChests) (map return [1..])
+        keysInChest = listArray (1,n) $ map (tail . tail) rawChests
+        keyOpening  = listArray (1,n) $ map     head      rawChests
         startChests = B.fromList [1..n]
         enoughKeys = all (>= 0) $ elems $ accumArray (+) 0 (1,200) $
-                     map (flip (,) 1) (iKeys ++ concat (elems keysInChest)) ++
-                     map (flip (,) (-1)) (map head chests)
-        r = guard enoughKeys >>
-            solve keysInChest chestsOpenedBy keyOpening iKeys startChests
-    putStrLn $ "Case #" ++ show i ++ ": " ++
-               maybe "IMPOSSIBLE" (unwords . map show) r
-
-solve keysInChest chestsOpenedBy keyOpening = go []
-  where go a keys lockedChests
-            | B.null lockedChests = Just (reverse a)
-            | dfs keys lockedChests = Nothing
-            | null cs = Nothing
-            | otherwise = foldr (<|>) Nothing $ flip map cs $ \c ->
-                          go (c:a) (keysInChest!c ++ delete (keyOpening!c) keys)
-                             (B.delete c lockedChests)
-            where cs = sort $
-                       filter (flip B.member lockedChests) $
+                     map (flip (,) 1) (startKeys ++ concat (elems keysInChest))
+                  ++ map (flip (,) (-1)) (elems keyOpening)
+        solve keys lockedChests trail
+            | B.null lockedChests = return (reverse trail)
+            | dfs keys lockedChests = fail "broken link"
+            | otherwise = cs >>= \c ->
+                          solve (keysInChest!c ++ delete (keyOpening!c) keys)
+                                (B.delete c lockedChests) (c : trail)
+            where cs = sort $ filter (flip B.member lockedChests) $
                        concatMap (chestsOpenedBy!) keys
         dfs _ cs | B.null cs = False
         dfs [] _             = True
         dfs (k:ks) cs        = dfs (ks' ++ ks) (cs B.\\ B.fromList cs')
             where cs' = filter (flip B.member cs) (chestsOpenedBy ! k)
                   ks' = concatMap (keysInChest!) cs'
+        result = guard enoughKeys >> solve startKeys startChests []
+    putStrLn $ "Case #" ++ show i ++ ": " ++
+               maybe "IMPOSSIBLE" (unwords . map show) (listToMaybe result)
