968cf211716cafe257fb7829600c4e672428fc78
Jean-Baptiste Mazon <jb@darkstar>
2013-05-04 20:21:43 +0200

    2013 Round 1B: A-large submitted

---
+++ 2013-1B-A-osmos-large.hs
@@ -0,0 +1,24 @@
+import Data.List
+import Control.Monad.State
+import qualified Data.Map.Strict as M
+main = do
+  t <- readLn
+  forM_ [1..t] $ \i -> do
+    [a,n] <- fmap (map read . words) getLine
+    ns <- fmap (map read . words) getLine
+    putStrLn $ "Case #" ++ show i ++ ": " ++
+               show (evalState (solve 0 a (sort ns)) M.empty)
+solve _ _ [] = return (0 :: Int)
+solve c a (n:ns) = do
+  m <- get
+  case M.lookup (c,a) m of
+    Just r -> return r
+    Nothing -> do
+      r <- if a > n then solve (c+1) (a+n) ns
+           else liftM2 min
+                  (fmap succ $ solve (c+1) a ns)
+                  (if a > 1 then fmap succ $ solve c (2*a - 1) (n:ns)
+                   else return maxBound)
+      modify (M.insert (c,a) r)
+      return r
+
