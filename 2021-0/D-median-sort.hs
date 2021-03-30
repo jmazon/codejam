0938ae2fc35ec03501c1cc023fa96a088aa76505
Jean-Baptiste Mazon <jb@yavin>
2021-03-30 22:27:10 +0200

    Consolidate for the bloggingz
    https://xn--sant-epa.ti-pun.ch/posts/2021-03-31-median-sort.html

--- 2021-0/D-median-sort.hs
+++ 2021-0/D-median-sort.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 
-import Control.Monad
-import Data.Foldable
+import Control.Monad (foldM,replicateM_,when,void)
+import Data.Coerce (coerce)
+import Data.Foldable (toList)
+import Data.Sequence (Seq)
 import qualified Data.Sequence as Q
-import System.Exit
-import System.IO
+import System.Exit (exitSuccess)
+import System.IO (hFlush,stdout)
 
 query :: [Int] -> IO Int
 query xs = do
@@ -15,39 +19,69 @@ query xs = do
   when (r < 0) exitSuccess
   pure r
 
+newtype Rank = Rank Int deriving (Eq,Ord,Enum,Num,Real,Integral)
+newtype Index = Index Int deriving (Enum)
+type Ranking = Seq Index
+
+insertAt :: Rank -> Index -> Ranking -> Ranking
+insertAt (Rank i) e q = Q.insertAt i e q
+
+index :: Ranking -> Rank -> Index
+index q (Rank r) = Q.index q r
+
+firstRank,lastRank :: Ranking -> Rank
+firstRank _ = Rank 0
+lastRank q = Rank (Q.length q - 1)
+
+submit :: Ranking -> IO ()
+submit = void . query . coerce . toList
+
+data Trisection = Before | Between | After
+
+medianOrder :: Ranking -> Rank -> Rank -> Index -> IO Trisection
+medianOrder q a b (Index i) = do
+  let Index sa = index q a
+      Index sb = index q b
+  m <- query [sa,sb,i]
+  if | m == sa -> pure Before
+     | m == sb -> pure After
+     | m ==  i -> pure Between
+
+insertionSort :: Int -> IO Ranking
+insertionSort n = foldM insert q0 [Index 3..Index n]
+  where q0 = Q.fromList [Index 1,Index 2]
+        insert q i = do
+          p <- ternarySearch q i
+          pure (insertAt p i q)
+
+ternarySearch :: Seq Index -> Index -> IO Rank
+ternarySearch q i = ternary (firstRank q) (succ (lastRank q)) where
+
+  -- seek an insertion position in [l,r]
+  ternary :: Rank -> Rank -> IO Rank
+  ternary l r
+    | w >= 2 = do
+        let a = l + w     `div` 3
+            b = l + w * 2 `div` 3
+        medianOrder q a b i >>= \case
+          Before  -> ternary  l    a
+          After   -> ternary (b+1) r
+          Between -> ternary (a+1) b
+    | w == 0 = pure l
+    | otherwise = binary l
+    where w = r - l
+
+  -- seek an insertion position among x and x+1
+  binary :: Rank -> IO Rank
+  binary x = do
+    let a = firstRank q
+        b = if x == a then lastRank q else x
+    medianOrder q a b i >>= \case
+      Before  -> pure a
+      Between -> pure $ if x == a then (a+1) else b
+      After   -> pure (b+1)
+
 main :: IO ()
 main = do
   [t,n,_] <- map read . words <$> getLine
-  replicateM_ t (medianSort n)
-  
-medianSort :: Int -> IO ()
-medianSort n = do
-  xs <- foldM insert (Q.fromList [1,2]) [3..n]
-  void $ query $ toList xs
-
-insert :: Q.Seq Int -> Int -> IO (Q.Seq Int)
-insert q x = go 0 (Q.length q - 1) where
-  go l r | l > r = pure (Q.insertAt l x q)
-         | l == r && l > 0 = do
-             let z = Q.index q 0
-                 a = Q.index q l
-             m <- query [a,z,x]
-             if | m == z -> error "WTF"
-                | m == x -> pure (Q.insertAt l x q)
-                | m == a -> pure (Q.insertAt (l+1) x q)
-         | l == r && l == 0 = do
-             let _ Q.:|> b = q
-                 a Q.:<| _ = q
-             m <- query [a,b,x]
-             if | m == a -> pure (Q.insertAt 0 x q)
-                | m == x -> pure (Q.insertAt 1 x q)
-                | m == b -> error "WTF"
-         | otherwise = do
-             let pa = l + (r - l) `div` 3
-                 pb = l + (2 * (r - l + 1)) `div` 3
-                 a = Q.index q pa
-                 b = Q.index q pb
-             m <- query [a,b,x]
-             if | m == a -> go l (pa-1)
-                | m == b -> go (pb+1) r
-                | m == x -> go (pa+1) (pb-1)
+  replicateM_ t (insertionSort n >>= submit)
