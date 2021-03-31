624f39fa199045154ede248e875b60bec2e74917
Jean-Baptiste Mazon <jb@yavin>
2021-03-31 22:39:27 +0200

    Write, debug and improve a solution to 2021 qualified problem E
    https://santé.ti-pun.ch/posts/2021-03-31-cheating-detection.html

---
+++ 2021-0/E-cheating-detection.hs
@@ -0,0 +1,67 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Main where
+
+import           Control.Monad (forM_)
+import           Data.Char     (digitToInt)
+import           Data.List     (sortOn)
+import           Data.Maybe    (catMaybes)
+import qualified Data.Vector as V
+import           Data.Vector   (Vector,(!),(!?))
+
+default (Int)
+
+readData :: IO (Vector (Vector Int))
+readData = V.replicateM 100 $ V.fromList . map digitToInt <$> getLine
+
+idCheater :: Vector (Vector Int) -> Int
+idCheater fs = cheater
+  where
+    pEase = V.sum <$> fs
+    pIndex = V.fromList $ map fst $ sortOn snd $ V.toList $ V.indexed pEase
+
+    qEase = V.foldl1' vAdd fs
+    qIndex = V.fromList $ map fst $ sortOn snd $ V.toList $ V.indexed qEase
+
+    ranked = for pIndex $ \i ->
+             for qIndex $ \j ->
+             fs ! i ! j
+
+    zoom = for ranked $ \qs ->
+           V.map V.sum $
+           V.unfoldrN 10 (Just . V.splitAt 1000) qs
+           -- instead of 100(100) previously
+
+    edges = ifor zoom $ \i z ->
+            fmap ((^3) . abs) $
+            vAvg $ catMaybes
+              [ (z `vSub`) <$> zoom !? (i-1)
+              , (z `vSub`) <$> zoom !? (i+1)
+              ]
+
+    cheater = pIndex ! V.maxIndex (V.sum <$> edges)
+
+var :: Vector Int -> Int
+var v = V.sum (V.map (^2) v) - (V.sum v)^2 `div` (V.length v)
+
+vAdd,vSub :: Vector Int -> Vector Int -> Vector Int
+vAdd = V.zipWith (+)
+vSub = V.zipWith (-)
+
+vAvg :: [Vector Int] -> Vector Int
+vAvg [v] = v
+vAvg [u,v] = (`div` 2) <$> vAdd u v
+
+for :: Vector a -> (a -> b) -> Vector b
+for  = flip  V.map
+
+ifor :: Vector a -> (Int -> a -> b) -> Vector b
+ifor = flip V.imap
+
+main :: IO ()
+main = do
+  t <- readLn
+  getLine -- p
+  forM_ [1..t] $ \i -> do
+    fs <- readData
+    putStrLn $ "Case #" ++ show i ++ ": " ++ show (1 + idCheater fs)
