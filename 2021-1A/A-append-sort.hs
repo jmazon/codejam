cb8b49afd14545b3e0483e662db338a645644143
Jean-Baptiste Mazon <jb@yavin>
2021-08-25 12:12:20 +0200

    Refined 2021-1A-A Append Sort for the bloggingz
    https://santé.ti-pun.ch/posts/2021-08-25-append-sort.html

--- 2021-1A/A-append-sort.hs
+++ 2021-1A/A-append-sort.hs
@@ -4,13 +4,39 @@ import Data.Function (on)
 import Data.List     (isPrefixOf)
 
 data Number = N
-  { nRaw :: [Int]
-  , nCooked :: Integer
-  , nLength :: Int
+  { nRaw    :: [Int]    -- ^ digits
+  , nCooked :: Integer  -- ^ value
+  , nWidth  :: Int      -- ^ width
   }
 
 fromString :: String -> Number
 fromString s = N (digitToInt <$> s) (read s) (length s)
+
+instance Eq Number where (==) = (==) `on` nCooked
+instance Ord Number where compare = compare `on` nCooked
+
+nbSucc :: Number -> Number
+nbSucc (N _ cooked _) = N ds cooked' (length ds)
+  where cooked' = succ cooked
+        ds = digitToInt <$> show cooked'
+
+nbPrefixOf :: Number -> Number -> Bool
+nbPrefixOf = isPrefixOf `on` nRaw
+
+zeroPad :: Number -> Int -> Number
+zeroPad (N raw cooked width) pad =
+  N (raw ++ replicate pad 0) (cooked * 10^pad) (width + pad)
+
+solve :: [Number] -> Int
+solve (x:xs) = snd $ foldl f (x,0) xs where
+  f (prev,acc) next
+    | prev < next = (next,acc)
+    | n' <- nbSucc prev, next `nbPrefixOf` n' = (n',acc + nWidth n' - nWidth next)
+    | prev < paddedSmall = (paddedSmall,acc + padWidth)
+    | otherwise          = (paddedLarge,acc + padWidth + 1)
+    where padWidth = nWidth prev - nWidth next
+          paddedSmall = zeroPad next padWidth
+          paddedLarge = zeroPad paddedSmall 1
 
 main :: IO ()
 main = do
@@ -19,30 +45,3 @@ main = do
     void getLine -- n
     xs <- map fromString . words <$> getLine
     putStrLn $ "Case #" ++ show i ++ ": " ++ show (solve xs)
-
-solve :: [Number] -> Int
-solve (x:xs) = snd $ foldl f (x,0) xs where
-  f (prev,acc) next
-    | prev < next = (next,acc)
-    | n' <- succ prev, next `nbPrefixOf` n' = (n',acc + nLength n' - nLength next)
-    | prev < smallPad = (smallPad,acc + pad)
-    | otherwise = (largePad,acc + pad + 1)
-    where pad = nLength prev - nLength next
-          smallPad = zeroPad next pad
-          largePad = zeroPad smallPad 1
-
-instance Eq Number where (==) = (==) `on` nCooked
-instance Ord Number where compare = compare `on` nCooked
-instance Enum Number where
-  toEnum = fromInt . fromIntegral
-  fromEnum = fromIntegral . nCooked
-
-fromInt :: Integer -> Number
-fromInt n = let s = show n in N (digitToInt <$> s) n (length s)
-
-nbPrefixOf :: Number -> Number -> Bool
-nbPrefixOf = isPrefixOf `on` nRaw
-
-zeroPad :: Number -> Int -> Number
-zeroPad (N raw cooked lg) pad =
-  N (raw ++ replicate pad 0) (cooked * 10^pad) (lg+pad)
