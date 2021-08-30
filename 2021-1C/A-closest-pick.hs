f6125133a25a2ac1610647e67b9e896c6d201527
Jean-Baptiste Mazon <jb@yavin>
2021-08-30 12:27:36 +0200

    Refined 2021-1C-A Closest Pick for the bloggingz
    https://xn--sant-epa.ti-pun.ch/posts/2021-08-28-closest-pick.html

--- 2021-1C/A-closest-pick.hs
+++ 2021-1C/A-closest-pick.hs
@@ -2,26 +2,24 @@ import Control.Monad (forM_)
 import Data.List (sort,sortOn)
 import Data.Ord (Down(Down))
 
+solve :: Int -> [Int] -> Double
+solve k ps =
+  let
+    single l r = (r - l) `div` 2
+    atStart = head ps - 1
+    atEnd = k - last ps
+    singles = atStart : atEnd : zipWith single ps (tail ps)
+    double l r = r - l - 1
+    doubles = zipWith double ps (tail ps)
+  in
+    fromIntegral (max (maximum (0 : doubles))
+                      (sum $ take 2 $ sortOn Down singles))
+    / fromIntegral k
+
 main :: IO ()
 main = do
-  t <- readLn
+  t <- readLn :: IO Int
   forM_ [1..t] $ \i -> do
     [_,k] <- map read . words <$> getLine
     ps <- map read . words <$> getLine
     putStrLn $ "Case #" ++ show i ++ ": " ++ show (solve k (sort ps))
-
-solve :: Int -> [Int] -> Double
-solve k ps =
-  fromIntegral (max (sum $ take 2 $ sortOn Down singles) (maximum doubles))
-  / fromIntegral k
-  where
-    singles = atStart : atEnd : map single rs
-    atStart = head ps - 1
-    atEnd = k - last ps
-    doubles = 0 : map double rs
-    single (a,b) = 1 + (b-a) `div` 2
-    double (a,b) = b - a + 1
-    rs = ranges ps
-    ranges (x:y:zs) | x+1 <= y-1 = (x+1,y-1) : ranges (y:zs)
-                    | otherwise =             ranges (y:zs)
-    ranges _ = []
